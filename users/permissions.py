f1c945ef86306884233c644ff8b9a07d242725f1
User1 <431410@mail.ru>
2024-10-17 17:32:12 +0900

    add permisson

---
+++ users/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions
+
+
+class IsOwnerPermission(permissions.BasePermission):
+    """Определяет, является ли пользователь владельцем объекта"""
+    def has_object_permission(self, request, view, obj):
+        if obj.owner == request.user:
+            return True
+        return False
