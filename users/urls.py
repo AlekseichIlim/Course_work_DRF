5aaec9279d37c805289e9bc333d0759eea07149d
User1 <431410@mail.ru>
2024-10-17 21:30:05 +0900

    corrected

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,8 @@ from users.views import (
     UserListAPIView,
     UserCreateAPIView,
     UserRetrieveAPIView,
-    UserUpdateAPIView, UserDestroyAPIView,
+    UserUpdateAPIView,
+    UserDestroyAPIView,
 )
 
 from users.apps import UsersConfig
@@ -17,10 +18,17 @@ app_name = UsersConfig.name
 urlpatterns = [
     path("", UserListAPIView.as_view(), name="user_list"),
     path("register/", UserCreateAPIView.as_view(), name="user_register"),
-    path("login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name="login",),
-    path("token/refresh/", TokenRefreshView.as_view(permission_classes=(AllowAny,)), name="token_refresh",),
+    path(
+        "login/",
+        TokenObtainPairView.as_view(permission_classes=(AllowAny,)),
+        name="login",
+    ),
+    path(
+        "token/refresh/",
+        TokenRefreshView.as_view(permission_classes=(AllowAny,)),
+        name="token_refresh",
+    ),
     path("<int:pk>/", UserRetrieveAPIView.as_view(), name="user_detail"),
     path("<int:pk>/update/", UserUpdateAPIView.as_view(), name="user_update"),
     path("<int:pk>/delete/", UserDestroyAPIView.as_view(), name="user_delete"),
-
 ]
