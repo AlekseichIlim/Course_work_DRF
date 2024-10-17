dcd1ce1170426b7b99cbd32f93c63ef7b5691e10
User1 <431410@mail.ru>
2024-10-17 12:56:02 +0900

    validation

--- habits/validators.py
+++ habits/validators.py
@@ -1,28 +1,27 @@
+from rest_framework import serializers
 from rest_framework.serializers import ValidationError
 
-from course_work_DRF.habits.models import Habit
+from habits.models import Habit
 
 
 class HabitsValidator:
+    """ Валидация полей привычки """
     def __init__(self, field):
         self.field = field
 
     def __call__(self, value):
         val = dict(value)
+        if val.get('related_habit'):
+            rel_hab = val.get('related_habit')
+            id_rel_hab = rel_hab.id
+            rel_hab = Habit.objects.all().get(pk=id_rel_hab)
+            if rel_hab.pleasant is False:
+                raise ValidationError(f'Связанной привычкой может быть только приятная привычка!')
         if val.get('complete_time') > 2:
             raise ValidationError('Время выполнения должно быть не более 2 минут!')
         elif val.get('pleasant') is False and (val.get('related_habit') and val.get('prize')):
             raise ValidationError('Для полезной привычки необходимо указать только связанную привычку или вознаграждение!')
-        elif value['pleasant'] is True and (value['prize'] or value['related_habit']):
+        elif val.get('pleasant') is True and (val.get('prize') or val.get('related_habit')):
             raise ValidationError('Для приятной привычки не может быть связанной привычки или вознаграждения!')
-        elif value['period'] > 7:
+        elif val.get('period') > 7:
             raise ValidationError('Нельзя выполнять привычку реже, чем раз в 7 дней!')
-        # elif val['related_habit'] is not None:
-        #     rel_hab = val['related_habit']
-        #     if rel_hab['pleasant'] is False:
-        #         raise ValidationError('Связанной привычкой может быть только приятная привычка!')
-
-def validate_related_habit(value):
-    habit = Habit.objects.all()
-    if item not in value:
-        raise ValidationError(f'Ссылка не корректна. Используйте материалы с {item}.')
