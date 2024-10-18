7d54f06d084111cbd514f5c83893645de49b66fe
User1 <431410@mail.ru>
2024-10-18 09:52:00 +0900

    corrected

--- config/settings.py
+++ config/settings.py
@@ -90,11 +90,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "habits",
-        "USER": "postgres",
-        "PASSWORD": "1001",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+        'NAME': os.getenv("NAME"),
+        'USER': os.getenv("USER"),
+        'PASSWORD': os.getenv("PASSWORD"),
+        'HOST': os.getenv("HOST"),
+        'PORT': os.getenv("PORT"),
     }
 }
 
